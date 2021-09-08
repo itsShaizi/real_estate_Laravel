4bc332dfc04c3e5adf63f1c64f6c2262c9ad2990
SunjidShibly <sun8496@gmail.com>
2021-09-09 02:44:09 +0600

    Comment form

--- resources/views/blog/view.blade.php
+++ resources/views/blog/view.blade.php
@@ -196,25 +196,33 @@
                                                         </ul>
                                                     </div>
                                                 </div>
+
                                                 <div class="bg-gray-50 px-4 py-6 sm:px-6">
+                                                    <div class="text-center">Want to join the discussion? <br>Feel free to contribute!</div>
                                                     <div class="flex space-x-3">
                                                         <div class="flex-shrink-0">
                                                             <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80" alt="" />
                                                         </div>
                                                         <div class="min-w-0 flex-1">
                                                             <form action="#">
-                                                                <div>
+                                                                <div class="pt-2">
                                                                     <label for="comment" class="sr-only">About</label>
-                                                                    <textarea id="comment" name="comment" rows="3" class="shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 sm:text-sm border border-gray-300 rounded-md" placeholder="Add a note"></textarea>
+                                                                    <textarea required id="comment" name="comment" rows="3" class="shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 sm:text-sm border border-gray-300 rounded-md" placeholder="Add a comment"></textarea>
+                                                                </div>
+                                                                <div class="pt-2">
+                                                                    <label for="name" class="sr-only">Name</label>
+                                                                    <input required id="name" type="text" name="name" class="shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 border border-gray-300 rounded-md" placeholder="Name">
+                                                                </div>
+                                                                <div class="pt-2">
+                                                                    <label for="email" class="sr-only">Email</label>
+                                                                    <input required id="email" type="email" name="email" class="shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 border border-gray-300 rounded-md" placeholder="Email">
+                                                                </div>
+                                                                <div class="pt-2">
+                                                                    <label for="website" class="sr-only">Website</label>
+                                                                    <input id="website" type="texy" name="website" class="shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 border border-gray-300 rounded-md" placeholder="Website">
                                                                 </div>
                                                                 <div class="mt-3 flex items-center justify-between">
-                                                                    <a href="#" class="group inline-flex items-start text-sm space-x-2 text-gray-500 hover:text-gray-900">
-                                                                        <!-- Heroicon name: solid/question-mark-circle -->
-                                                                        <svg class="flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" >
-                                                                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
-                                                                        </svg>
-                                                                        <span> Some HTML is okay. </span>
-                                                                    </a>
+
                                                                     <button type="submit" class=" inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"> Comment
                                                                     </button>
                                                                 </div>

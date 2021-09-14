764a3faeefed263cbb9dbdeedcfb54f39e57e059
zubairhafeez <zubairhafeez56@gmail.com>
2021-09-15 02:16:46 +0500

    Chane search button color

--- resources/views/components/blog/search.blade.php
+++ resources/views/components/blog/search.blade.php
@@ -3,7 +3,7 @@
 <div class="relative">
     <input type="text" class="border border-gray-200 h-14 w-80 pl-10 pr-20 z-0 focus:shadow focus:outline-none" placeholder="Search anything..." />
     <div class="absolute top-0 right-0">
-        <button class="h-14 w-14 text-white bg-indigo-500 hover:bg-indigo-600">
+        <button class="h-14 w-14 text-white bg-indigo-400 hover:bg-indigo-300">
             <svg class="w-6 h-6 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
             </svg>
